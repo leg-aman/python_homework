f698e83668e6e0fce12b7dac2139f3c773ff5593
leg-aman <126026416+leg-aman@users.noreply.github.com>
2025-03-19 17:16:18 -0400

    assignment1 first commit

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -1,1 +1,80 @@
 # Write your code here.
+
+# Task 1: Hello
+def hello():
+    return "Hello!"
+
+# Task 2: Greet with a Formatted String
+def greet(name):
+    return "Hello, "+ name + "!"
+
+# Task 3: Calculator
+def calc(arg1,arg2,arg3="multiply"):
+    if type(arg1) == str or type(arg2) == str:
+        return "You can't multiply those values!"
+    if arg3 == "add":
+        return arg1 + arg2
+    if arg3 == "subtract":
+        return arg1 - arg2
+    if arg3 == "multiply":
+        return arg1 * arg2
+    if arg3 == "divide":
+        if arg2 == 0:
+            return "You can't divide by 0!"
+        elif type(arg1) == int and type(arg2) == int:
+            return arg1//arg2
+        else:
+            return arg1 / arg2
+    if arg3 == "modulo":
+        return arg1 % arg2
+    if arg3 == "power":
+        return arg1 ** arg2
+    
+# Task 4: Data Type Conversion
+def data_type_conversion(arg1,arg2):
+    if arg2 == "int":
+        try:
+            int(arg1)
+        except ValueError:
+            return f"You can't convert {str(arg1)} into a {str(arg2)}."
+        else:
+            return int(arg1)
+    if arg2 == "float":
+        try:
+            float(arg1)
+        except ValueError:
+            return f"You can't convert {str(arg1)} into a {str(arg2)}."
+        else:
+            return float(arg1)
+    if arg2 == "str":
+        return str(arg1)
+    
+# Task 5: Grading System, Using *args
+def grade(*args):
+    try:
+        result = sum(args)//len(args)    
+    except TypeError:
+        return "Invalid data was provided."
+    else:
+        result = sum(args)//len(args)
+        if result >= 90:
+            return "A"
+        elif result >= 80 or result <= 89:
+            return "B"
+        elif result >= 70 or result <= 79:
+            return "C"
+        elif result >= 60 or result <= 69:
+            return "D"
+        elif result <60:
+            return "F"
+        
+# Task 6: Use a For Loop with a Range
+def repeat(word, count):
+    repeatedWord = ''
+    for i in range(count):
+        repeatedWord += word
+    return repeatedWord
+# Task 7: Student Scores, Using **kwargs
+def student_scores(*args,**kwargs):
+    if args[0] == "mean":
+        print( type(kwargs.values))
