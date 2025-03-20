f2150faec103557c2ad196e99dc33076d8534220
leg-aman <126026416+leg-aman@users.noreply.github.com>
2025-03-20 10:36:52 -0400

    assignment1 task 7 - 9

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -6,7 +6,7 @@ def hello():
 
 # Task 2: Greet with a Formatted String
 def greet(name):
-    return "Hello, "+ name + "!"
+    return f"Hello, {name}!"
 
 # Task 3: Calculator
 def calc(arg1,arg2,arg3="multiply"):
@@ -76,5 +76,32 @@ def repeat(word, count):
     return repeatedWord
 # Task 7: Student Scores, Using **kwargs
 def student_scores(*args,**kwargs):
-    if args[0] == "mean":
-        print( type(kwargs.values))
+    if "best" in args:
+        for key, value in kwargs.items():
+            if value == max(kwargs.values()):
+                return key
+    elif "mean" in args:
+        return sum(list(kwargs.values()))/len(kwargs.values()) 
+    
+# Task 8: Titleize, with String and List Operations
+def titleize(words):
+    word_list = words.split()
+    book_title = []
+    for i,word in enumerate(word_list):
+        if i == 0 or i == len(word_list) - 1:
+            book_title.append(word[0].capitalize() + word[1:])
+        elif word in ["a", "on", "an", "the", "of", "and", "is", "in"]:
+            book_title.append(word)
+        else:
+             book_title.append(word[0].capitalize() + word[1:])
+    return " ".join(book_title)
+
+# Task 9: Hangman, with more String Operations
+def hangman(secret_word,guessed_word):
+    guessed_letter = ''
+    for i in secret_word:
+        if i in guessed_word:
+            guessed_letter += i
+        else:
+            guessed_letter += "_"
+    return guessed_letter
