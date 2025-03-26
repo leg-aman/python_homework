cf324c2ef26a9c7f4ad90c864b4958a6fdce83d2
leg-aman <126026416+leg-aman@users.noreply.github.com>
2025-03-25 22:43:39 -0400

    assignment2

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -25,6 +25,33 @@ def column_index(args):
         return employees["fields"].index(str(args))
 employee_id_column = column_index("employee_id")
 
+# Task 4: Find the Employee First Nameimport csv
+import traceback
+import os
+
+# Task 2: Read a CSV File
+def read_employees ():
+    column_header = {}
+    list_of_rows = []
+    try:
+        with open('../csv/employees.csv','r', newline='') as file:
+            reader = csv.reader(file)
+            header = next(reader)
+            column_header['fields'] = header
+            for row in reader:
+                list_of_rows.append(row)
+            column_header['rows'] = list_of_rows
+    except Exception as e:
+        handle_exception(e)
+    return column_header
+employees = read_employees()
+# print(employees)
+
+# Task 3: Find the Column Index
+def column_index(args):
+        return employees["fields"].index(str(args))
+employee_id_column = column_index("employee_id")
+
 # Task 4: Find the Employee First Name
 def first_name(row_num):
     employee_first_name_column = column_index("first_name")
@@ -51,9 +78,62 @@ sort_by_last_name()
 
 # Task 8: Create a dict for an Employee
 def employee_dict(args):
-     zipped = dict(zip(employees['fields'],args))
-     dict_without_empId = zipped.pop("employee_id")
-     return dict_without_empId
+     row_dict = dict(zip(employees['fields'],args))
+     if 'employee_id' in row_dict:
+            del row_dict['employee_id']
+            return row_dict
+
+# Task 10: Use the os Module
+def get_this_value():
+    return os.getenv('THISVALUE')
+
+
+
+
+
+
+
+
+
+def handle_exception(e):
+        trace_back = traceback.extract_tb(e.__traceback__)
+        stack_trace = list()
+        for trace in trace_back:
+            stack_trace.append(f'File : {trace[0]} , Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
+        print(f"Exception type: {type(e).__name__}")
+        message = str(e)
+        if message:
+            print(f"Exception message: {message}")
+        print(f"Stack trace: {stack_trace}")
+def first_name(row_num):
+    employee_first_name_column = column_index("first_name")
+    return employees['rows'][row_num][employee_first_name_column]
+
+# Task 5: Find the Employee: a Function in a Function
+def employee_find(employee_id):
+    def employee_match(row):
+       return int(row[employee_id_column]) == employee_id
+    matches=list(filter(employee_match, employees["rows"]))
+    return matches
+
+# Task 6: Find the Employee with a Lambda
+def employee_find_2(employee_id):
+    matches = list(filter(lambda row : int(row[employee_id_column]) == employee_id , employees["rows"]))
+    return matches
+     
+# Task 7: Sort the Rows by last_name Using a Lambda
+def sort_by_last_name():
+    last_name_index = column_index("last_name")
+    employees['rows'].sort(key=lambda row: row[last_name_index])
+    return employees['rows']
+sort_by_last_name()
+
+# Task 8: Create a dict for an Employee
+def employee_dict(args):
+     row_dict = dict(zip(employees['fields'],args))
+     if 'employee_id' in row_dict:
+            del row_dict['employee_id']
+            return row_dict
 
 # Task 10: Use the os Module
 def get_this_value():
