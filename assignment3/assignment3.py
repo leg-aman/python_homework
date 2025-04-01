73bd0823d24b2d69092028348b7634dc46eadd65
leg-aman <126026416+leg-aman@users.noreply.github.com>
2025-04-01 08:17:36 -0400

    assignment3

---
+++ assignment3/assignment3.py
@@ -0,0 +1,19 @@
+# Task 1: Introduction to Pandas - Creating and Manipulating DataFrames
+import pandas as pd
+
+# Create a DataFrame from a dictionary:
+data ={
+    'Name': ['Alice', 'Bob', 'Charlie'],
+    'Age': [25, 30, 35],
+    'City': ['New York', 'Los Angeles', 'Chicago']
+}
+task1_data_frame = pd.DataFrame(data)
+print(task1_data_frame)
+
+# Add a new column:
+task1_with_salary = task1_data_frame.copy()
+task1_with_salary['Salary'] = [70000, 80000, 90000]
+print(task1_with_salary)
+
+# Modify an existing column:
+task1_older = task1_with_salary.copy()
