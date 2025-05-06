405bcac521524b7492518a7cb477b8b854a222fd
leg-aman <126026416+leg-aman@users.noreply.github.com>
2025-05-06 11:42:14 -0400

    Task 2: understanding Subqueries

--- assignment8/advanced_sql.py
+++ assignment8/advanced_sql.py
@@ -12,6 +12,7 @@ try:
         print("Database connected successfully!")
         cursor = conn.cursor()
         conn.execute("PRAGMA foreign_keys = 1")
+
 # Tak 1: Complex JOINs with Aggregation
         cursor.execute("""
                 SELECT orders.order_id AS ordered_id,
@@ -20,16 +21,32 @@ try:
                 JOIN line_items ON orders.order_id = line_items.order_id 
                 JOIN products ON line_items.product_id = products.product_id 
                 GROUP BY orders.order_id 
-                ORDER BY orders.order_id LIMIT 5;
+                ORDER BY orders.order_id LIMIT 5
                 """)
         results = cursor.fetchall()
         print("Results of the complex JOINs with Aggregation:")
         for row in results:
             print(row)
 
-
-
-
+# Task 2: Understanding Subqueries
+        cursor.execute("""
+            SELECT c.customer_name, AVG(order_total.total_price) AS average_total_price
+            FROM customers c
+            LEFT JOIN (
+                SELECT o.customer_id AS customer_id_b,
+                       SUM( li.quantity * p.price ) AS total_price
+                FROM orders o
+                JOIN line_items li ON o.order_id = li.order_id
+                JOIN products p ON li.product_id = p.product_id
+                GROUP BY o.customer_id
+            ) AS order_total
+            ON c.customer_id = order_total.customer_id_b
+            GROUP BY c.customer_id;
+        """)
+        res = cursor.fetchall()
+        print("Results of the subquery:")
+        for row in res:
+            print(row)
 
 except sqlite3.Error as e:
     print(f"An error occurred: {e}")
