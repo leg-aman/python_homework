c60a8547cd2dadd29e52e53912214d3f2bb08a58
leg-aman <126026416+leg-aman@users.noreply.github.com>
2025-06-03 16:14:24 -0400

    Building Dashboards with Dash

---
+++ assignment11/employee_results.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import sqlite3
+def load_employee_results():
+    conn = sqlite3.connect('../db/lesson.db')
+    query = "SELECT last_name, SUM(price * quantity) AS revenue FROM employees e JOIN orders o ON e.employee_id = o.employee_id JOIN line_items l ON o.order_id = l.order_id JOIN products p ON l.product_id = p.product_id GROUP BY e.employee_id"
+    employee_results = pd.read_sql_query(query, conn)
+    conn.close()
+    return employee_results
+# print(load_employee_results())
+employee_results = load_employee_results()
+print(employee_results)
